8bfa780112c5d0a839e044c3698aadaa42bedde7
TCosta <tainacosta@infojr.com.br>
2025-03-16 07:37:16 -0300

    refactor: Crie o diretório pages e mova os arquivos da homepage para lá

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import Formulario from './components/Formulario';
-import Lista from './components/Lista'
+import Formulario from '../components/Formulario';
+import Lista from '../components/Lista'
+import './style.scss'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">        
+      <header className="AppStyle">        
         <Formulario />
         <Lista />
       </header>
