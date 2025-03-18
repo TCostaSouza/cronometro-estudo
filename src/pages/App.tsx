53f98d8298f5bc3af7ebc039af8d13510281fb90
TCosta <tainacosta@infojr.com.br>
2025-03-18 14:31:01 -0300

    feat: Adicione Cronômetro à página App

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Formulario from '../components/Formulario';
 import Lista from '../components/Lista'
 import style from './App.module.scss'
+import Cronometro from '../components/Cronometro';
 
 function App() {
   return (
     <div className="App">
       <header className={style.AppStyle}>        
         <Formulario />
+        <Cronometro />
         <Lista />
       </header>
     </div>
