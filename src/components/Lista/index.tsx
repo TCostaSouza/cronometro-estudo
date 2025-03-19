04bb5bd559656bb1541d3698c065172d3d19e79b
TCosta <tainacosta@infojr.com.br>
2025-03-19 18:53:14 -0300

    feat: Atualize a lista de tarefas para que ela comece vazia

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,25 +1,15 @@
 import React from 'react';
-import style from './Lista.module.scss'
-import Item from './item';
+import style from './Lista.module.scss';
+import Item from './Item/index';
+import { ITarefa } from '../../types/tarefa';
 
-function Lista() {
-  const tarefas = [
-    {
-      tarefa: 'React',
-      tempo: '02:00:00',
-    },
-    {
-      tarefa: 'JavaScript',
-      tempo: '01:00:00',
-    },{
-        tarefa: 'TypeScript',
-        tempo: '03:00:00'
-    }
-  ];
 
+function Lista( { tarefas }: { tarefas:ITarefa[] } ) {
   return (
     <aside className={style.listaTarefas}>
-      <h2>Estudos do dia</h2>
+      <h2>
+        Estudos do dia
+      </h2>
       <ul>
         {tarefas.map((item, index) => (
           <Item key={index} {...item} />
