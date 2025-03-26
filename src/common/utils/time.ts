76a8f67b7102a9a79e88f9935ece1f0115993bae
TCosta <tainacosta@infojr.com.br>
2025-03-26 18:57:46 -0300

    perf: Adicione código à função auxiliar tempoParaSegundos para lidar com entradas que tenham apenas minutos

--- src/common/utils/time.ts
+++ src/common/utils/time.ts
@@ -1,5 +1,16 @@
-export function tempoParaSegundos (tempo: string) {
-    const [horas, minutos, segundos] = tempo.split(':')
+export function tempoParaSegundos(tempo: string) {
+  if (!tempo || tempo.split(':').length !== 3) {
+    const [minutos, segundos] = tempo.split(':');
+    const minutosEmSegundos = Number(minutos) * 60;
 
-    return Number(horas) * 3600 + Number(minutos) * 60 + Number(segundos)
+    return minutosEmSegundos + Number(segundos);
+  } else {
+    const [horas, minutos, segundos] = tempo.split(':');
+
+    const horasEmSegundos = Number(horas) * 3600;
+    const minutosEmSegundos = Number(minutos) * 60;
+
+    return horasEmSegundos + minutosEmSegundos + Number(segundos);
+  }
+
 }
