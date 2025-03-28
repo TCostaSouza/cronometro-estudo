1ff90b670e32c61f0aeb0a81a4fe8cd422107a36
TCosta <tainacosta@infojr.com.br>
2025-03-28 18:10:49 -0300

    feat: Adicione o atributo tempo ao relógio do cronômetro e converta o valor recebido do item selecionado para minutos e segundos

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -27,7 +27,7 @@ export default function Cronometro({ selecionado }:Props) {
     <div className={style.cronometro}>
       <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
       <div className={style.relogioWrapper}>
-        <Relogio />
+        <Relogio tempo = { tempo }/>
       </div>
       <Botao>Começar</Botao>
     </div>
