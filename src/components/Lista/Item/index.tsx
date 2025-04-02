3ba2762f87a38e955283f2b5353c12df36970236
TCosta <tainacosta@infojr.com.br>
2025-04-01 22:58:56 -0300

    feat: Faça com que um check apareça no item completado

--- src/components/Lista/Item/index.tsx
+++ src/components/Lista/Item/index.tsx
@@ -16,7 +16,9 @@ export default function Item({
 }: Props) {
   return (
     <li
-      className={`${style.item} ${selecionado ? style.itemSelecionado : ''} ${completado ? style.itemCompletado : ''}`}
+      className={`${style.item} ${selecionado ? style.itemSelecionado : ''} ${
+        completado ? style.itemCompletado : ''
+      }`}
       onClick={() =>
         selecionarTarefa({
           tarefa,
@@ -29,6 +31,11 @@ export default function Item({
     >
       <h3>{tarefa}</h3>
       <span>{tempo}</span>
+      {completado && (
+        <span 
+          className={style.concluido}
+          aria-label="tarefa completada" ></span>
+      )}
     </li>
   );
 }
