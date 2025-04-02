9ba6076de524b1cd0a2d195a51c520752c9a7715
TCosta <tainacosta@infojr.com.br>
2025-04-01 23:01:20 -0300

    feat: Faça com que o item só possa ser selecionado se ele não tiver sido completado

--- src/components/Lista/Item/index.tsx
+++ src/components/Lista/Item/index.tsx
@@ -20,7 +20,7 @@ export default function Item({
         completado ? style.itemCompletado : ''
       }`}
       onClick={() =>
-        selecionarTarefa({
+        !completado && selecionarTarefa({
           tarefa,
           tempo,
           selecionado,
